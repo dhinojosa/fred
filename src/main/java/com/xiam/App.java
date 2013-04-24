ac7b4f19fbfddec695212b702ffbca2e8c9ce267
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2013-04-24 15:50:56 +0100

    added pom.xml & src

---
+++ src/main/java/com/xiam/App.java
@@ -0,0 +1,18 @@
+package com.xiam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println("One");
+        System.out.println("Two");
+        System.out.println("Three");
+        System.out.println("Four");
+    }
+}
